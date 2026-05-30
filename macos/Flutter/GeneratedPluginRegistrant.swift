cf704f9a8132094b8b03c3a95cfed4c54bf36dfa
Ali Al Majid <alimajid84499@gmail.com>
2026-05-30 16:48:57 +0700

    refactor: remove audioplayers dependency and regenerate platform plugin configurations

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< Updated upstream
 import audioplayers_darwin
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+=======
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+>>>>>>> Stashed changes
 }
